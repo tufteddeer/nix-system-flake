f4998b547a73a0df5127e407ccb202180fa3014f
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-06-11 16:55:00 +0200

    initial notebook flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+
+    lib = nixpkgs.lib;
+
+  in {
+    nixosConfigurations = {
+      normandy = lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+        ];
+      };
+      
+    };
+  };
+}
