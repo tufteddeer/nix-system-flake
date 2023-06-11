9ac6d644ca1c19f93b143087bee207d3d612a3de
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-06-11 17:40:32 +0200

    fix wireless assertition

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Enable networking
   networking.networkmanager.enable = true;
