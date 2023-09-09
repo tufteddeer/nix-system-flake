553fcb549e72b75d7fe6a16a0ddc5eed4dd2c93b
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-09-09 12:04:37 +0200

    Switch to unstable nixos

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
