b2ef8b9781992a7766ddb64833eda6af32c83766
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-09-09 12:05:26 +0200

    enable auto-optimise-store

--- nix-options.nix
+++ nix-options.nix
@@ -3,6 +3,8 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+  nix.settings.auto-optimise-store = true;
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 }
