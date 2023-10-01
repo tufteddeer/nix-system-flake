58b6fd9b9d5eded32ac05644529bda2d66b1a45e
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-10-01 17:26:15 +0200

    Add tray gnome extension

--- gnome.nix
+++ gnome.nix
@@ -5,7 +5,12 @@
 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  
+
+  environment.systemPackages = with pkgs; [
+    gnome-extension-manager
+    gnomeExtensions.appindicator
+  ];
+
   environment.gnome.excludePackages = (with pkgs; [
   gnome-photos
   gnome-tour
