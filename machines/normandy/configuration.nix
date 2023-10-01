42b2f3357252dc6480fe0e1dc239eb85db4d78fa
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-10-01 16:30:43 +0200

    Add mountpoint and borg repo for paperless backups

--- machines/normandy/configuration.nix
+++ machines/normandy/configuration.nix
@@ -24,23 +24,6 @@
   services.logind.lidSwitchExternalPower = "ignore";
   services.logind.lidSwitchDocked = "ignore";
 
-  users.users.borg = {
-    isNormalUser = true;
-    extraGroups = [ "freshrss" "f" ];
-  };
-
-  # TODO: only user "borg" should have read and write on this
-  fileSystems."/run/media/borg/backup" = {
-      device = "//192.168.178.2/home/Backups/Homeserver";
-      fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1001,gid=1001"];
-  };
-
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
--- services/paperless.nix
+++ services/paperless.nix
@@ -1,5 +1,9 @@
 { config, pkgs, ... }:
-rec {  services.paperless = {
+let
+  backup_repo = "/backup/paperless-backup";
+in
+rec {
+  services.paperless = {
     enable = true;
     address = "0.0.0.0";
     port = 58080;
@@ -9,16 +13,31 @@ rec {  services.paperless = {
   };
 
 
+  fileSystems."${backup_repo}" = {
+    device = "//192.168.178.2/home/Backups/Homeserver/paperless-backup";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=${toString config.users.users.paperless.uid}" ]; #",gid=${toString config.users.users.paperless.gid}"];
+  };
+
   services.borgbackup.jobs.paperless = {
     paths = "${services.paperless.dataDir}";
     encryption.mode = "none";
-    repo = "/run/media/borg/backup/borg_repo";
+    repo = backup_repo;
     compression = "zstd,1";
     startAt = "daily";
-    user = "borg";
+    user = "paperless";
+    group = "paperless";
 
     exclude = [
-      "${services.paperless.dataDir}/superuser-password" 
+      "${services.paperless.dataDir}/superuser-password"
+      "${services.paperless.dataDir}/superuser-state"
     ];
   };
 }
+
