a1d94aecbca217822c15d39e07022a82faf6863a
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-12-30 19:45:59 +0100

    docker adguard

--- services/adguard.nix
+++ services/adguard.nix
@@ -1,9 +1,20 @@
 { config, pkgs, ... }:
 {
-  services.adguardhome = {
-    enable = true;
-    settings.bind_host = "192.168.178.20";
-    settings.schema_version = 20;
-    openFirewall = true;
+
+  virtualisation.oci-containers.containers.adguardhome = {
+    image = "adguard/adguardhome:v0.107.41";
+    autoStart = true;
+    ports = [
+      "53:53/udp"
+      "3000:3000"
+    ];
+    volumes = [
+      "/volumes/adguard_workdir:/opt/adguardhome/work"
+      "/volumes/adguard_confdir:/opt/adguardhome/conf"
+    ];
   };
+
+  networking.firewall.allowedTCPPorts = [ 3000 ];
+
+  networking.firewall.allowedUDPPorts = [ 53 ];
 }
