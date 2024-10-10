0d62fa43f7f52ce5509759f189bdada24928c774
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2024-10-10 16:20:21 +0200

    jellyfin: read media files from nas

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -1,30 +1,27 @@
 { config, pkgs, ... }:
 {
 
-  # fileSystems."/media/NAS_Movies" = {
-  #   device = "//192.168.178.2/Movies";
-  #   fsType = "cifs";
-  #   options =
-  #     let
-  #       # this line prevents hanging on network split
-  #       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-  #     in
-  #     [ "${automount_opts},credentials=${config.sops.secrets.nas-media-credentials.path}" ];
-  # };
+  fileSystems."/media/NAS_Movies" = {
+     device = "//192.168.178.2/Movies";
+     fsType = "cifs";
+     options =
+       let
+         # this line prevents hanging on network split
+         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+       in
+       [ "${automount_opts},credentials=${config.sops.secrets.nas-media-credentials.path}" ];
+  };
 
-  # TODO: when the library is on the nas, clear the folder and mount it form the nas
-  # fileSystems."/media/NAS_Shows" = {
-    # device = "//192.168.178.2/Shows";
-    # fsType = "cifs";
-    # options =
-      # let
+ fileSystems."/media/NAS_Shows" = {
+   device = "//192.168.178.2/Shows";
+   fsType = "cifs";
+   options =
+     let
         # this line prevents hanging on network split
-        # automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      # in
-      # [ "${automount_opts},credentials=${config.sops.secrets.nas-media-credentials.path}" ];
-  # };
-
-
+       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in
+      [ "${automount_opts},credentials=${config.sops.secrets.nas-media-credentials.path}" ];
+  };
 
   environment.systemPackages = [
     pkgs.jellyfin
@@ -34,6 +31,5 @@
 
   services.jellyfin = {
     enable = true;
-    openFirewall = true;
   };
 }
