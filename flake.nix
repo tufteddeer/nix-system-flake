4ab1eaf62a71b725859010b25d977fec88cf2501
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2025-03-16 17:41:38 +0100

    upgrade to 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix.url = "github:Mic92/sops-nix";
